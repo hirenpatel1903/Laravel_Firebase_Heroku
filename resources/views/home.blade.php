0afb762bb432834fd062a08fa38ddfe59f8b0089
Hiren Patel <patelhiren.hp19@gmail.com>
2022-04-16 18:01:09 +0530

    changes and fixing error

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,8 +18,8 @@
                             <tr>
                                 <th scope="col">#</th>
                                 <th scope="col">Title</th>
-                                <th scope="col">Content</th>
-                                <th scope="col">Action</th>
+                                <th scope="col">Sub Title</th>
+                                <th scope="col">Authors</th>
                             </tr>
                         </thead>
                         <tbody id="table-list">
@@ -31,18 +31,46 @@
         </div>
     </div>
 </div>
+<div class="modal fade" id="add-modal" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Create Book</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" ></button>
+            </div>
+            <div class="modal-body">
+                <form id="add-post" method="post">
+                    <div class="mb-3">
+                        <label for="title" class="form-label">Title</label>
+                        <input type="text" class="form-control" id="title" name="title">
+                    </div>
+                    <div class="mb-3">
+                        <label for="subtitle" class="form-label">Sub Title</label>
+                        <input type="text" class="form-control" id="subtitle" name="subtitle">
+                    </div>
+                    <div class="mb-3">
+                        <label for="authors" class="form-label">Authors</label>
+                        <textarea class="form-control" id="authors" name="authors"></textarea>
+                    </div>
+                    <button type="button" id="add-submit" class="btn btn-primary">Submit</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 <script src="https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js"></script>
     <script src="https://www.gstatic.com/firebasejs/8.0.0/firebase-database.js"></script>
     <script type="text/javascript">
         // Your web app's Firebase configuration
         const firebaseConfig = {
-            apiKey: "{{ config('services.firebase.api_key') }}",
-            authDomain: "{{ config('services.firebase.auth_domain') }}",
-            databaseURL: "{{ config('services.firebase.database_url') }}",
-            projectId: "{{ config('services.firebase.project_id') }}",
-            storageBucket: "{{ config('services.firebase.storage_bucket') }}",
-            messagingSenderId: "{{ config('services.firebase.messaging_sender_id') }}",
-            appId: "{{ config('services.firebase.app_id') }}"
+            apiKey: "{{ config('services.firebase.apiKey') }}",
+            authDomain: "{{ config('services.firebase.authDomain') }}",
+            databaseURL: "{{ config('services.firebase.databaseURL') }}",
+            projectId: "{{ config('services.firebase.projectId') }}",
+            storageBucket: "{{ config('services.firebase.storageBucket') }}",
+            messagingSenderId: "{{ config('services.firebase.messagingSenderId') }}",
+            appId: "{{ config('services.firebase.appId') }}"
         };
 
         // Initialize Firebase
@@ -62,7 +90,8 @@
                     htmls.push('<tr>\
                         <td>'+ index +'</td>\
                         <td>'+ value.title +'</td>\
-                        <td>'+ value.content +'</td>\
+                        <td>'+ value.subtitle +'</td>\
+                        <td>'+ value.authors +'</td>\
                     </tr>');
                 }
                 lastId = index;
@@ -77,13 +106,15 @@
 
             firebase.database().ref('posts/' + createId).set({
                 title: formData[0].value,
-                content: formData[1].value,
+                subtitle: formData[1].value,
+                authors: formData[1].value,
             });
 
             // Reassign lastID value
             lastId = createId;
             $("#add-post")[0].reset();
             $("#add-modal").modal('hide');
+            window.location.reload();
         });
     </script>
 @endsection
